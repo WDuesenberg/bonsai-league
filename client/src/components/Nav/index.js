07039f34ff44cd5de7132b294b5a916eced498bc
w_duse89 <wyatt.duesenberg@gmail.com>
2019-11-14 01:57:25 -0700

    stuff

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import "./style.scss";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark" href="/">
       <a className="navbar-brand" href="/">
         Bonsai League
       </a>
